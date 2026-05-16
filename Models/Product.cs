16e5b9fbe2c343f9294bcbcad89599e5461ab801
Cral0202 <quantum.camel@proton.me>
2026-05-16 15:04:09 +0200

    UI updates

--- Models/Product.cs
+++ Models/Product.cs
@@ -1,4 +1,5 @@
 using System.Collections.ObjectModel;
+using System.Linq;
 
 namespace PriceTrail.Models;
 
@@ -7,4 +8,9 @@ public class Product
     public int Id { get; set; }
     public string Name { get; set; } = "";
     public ObservableCollection<ProductPage> ProductPages { get; set; } = [];
+
+    public ProductPage? LowestPricePage => ProductPages.MinBy(p => p.Price);
+    public decimal? LowestPrice => LowestPricePage?.Price;
+    public string LowestPriceCurrency => LowestPricePage?.Currency ?? "";
+    public string LowestPriceStore => LowestPricePage?.StoreName ?? "";
 }
